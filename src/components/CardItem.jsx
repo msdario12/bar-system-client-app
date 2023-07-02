5c69baf2f8ff3bef5f0f8d5e648ea64e7d40d24e
msdario12 <mansilladario94@gmail.com>
2023-07-01 21:37:15 -0300

    Fix some spaces and margin in card to show each item

--- src/components/CardItem.jsx
+++ src/components/CardItem.jsx
@@ -14,20 +14,21 @@ export const CardItem = (props) => {
 
 			<div className='p-4 col-span-4 grid'>
 				<div className='col-span-4 mb-6'>
-					<p className='text-sm text-slate-500'>{category}</p>
+					<p className='text-sm text-slate-400'>{category}</p>
 					<h5 className='text-xl font-semibold tracking-tight text-gray-900 dark:text-white'>
 						{name}
 					</h5>
-					<p className=' text-sm text-slate-400'>{description}</p>
+					<p className=' text-sm text-slate-300'>{description}</p>
 				</div>
 
-				<div className='grid grid-cols-6 col-span-4'>
+				<div className='grid grid-cols-6 col-span-4 max-h-12 my-auto'>
 					<span className='my-auto col-span-3 text-3xl  font-bold text-gray-900 dark:text-white'>
 						{`$ ${price}`}
 					</span>
+
 					<a
 						href='#'
-						className='text-white col-span-3 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-3 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800'>
+						className='text-white col-span-3 flex flex-col align-middle justify-center bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-3 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800'>
 						Añadir al carrito
 					</a>
 				</div>
