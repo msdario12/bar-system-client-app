dfe0dc2d6da3f79d50e8c5ab9c51c1b1bcda625f
msdario12 <mansilladario94@gmail.com>
2023-07-01 21:29:08 -0300

    Added format to card to show each item

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,43 @@
+import { CardItem } from './components/CardItem';
+import { menuItems } from './db/menuItems';
 export const App = () => {
 	return (
-		<div>
+		<div className='container mx-auto px-3'>
+			<header>
+				<div className='logo '></div>
+				<nav>
+					<ul>
+						<li>Home</li>
+						<li>Como funciona</li>
+						<li>Carrito</li>
+						<li>Generar QR</li>
+					</ul>
+				</nav>
+			</header>
 			<h1 className='text-xl font-bold'>Aplicación para clientes</h1>
+			<section className='min-h-screen grid content-center'>
+				<h2 className='text-5xl font-bold'>
+					Bienvenido al menu digital de Bar RRT
+				</h2>
+			</section>
+			<section className='min-h-screen grid content-center'>
+				<h2 className='text-3xl font-bold'>Instrucciones para el uso</h2>
+				<p>
+					Aca se explicaría como funciona el sistema y como debe operar el
+					cliente en simples pasos
+				</p>
+			</section>
+			<section>
+				<h2 className='text-3xl font-bold'>Buscador, categorias, ofertas</h2>
+			</section>
+			<section>
+				<h2 className='text-3xl font-bold'>Carta</h2>
+				<div>
+					<h3 className='text-2xl'>Bebidas</h3>
+					{menuItems &&
+						menuItems.map((item) => <CardItem key={item.id} item={item} />)}
+				</div>
+			</section>
 		</div>
 	);
 };
---
+++ src/components/CardItem.jsx
@@ -0,0 +1,37 @@
+export const CardItem = (props) => {
+	const { item } = props;
+	const { name, price, description, category, image } = item;
+
+	return (
+		<div className='w-full grid grid-cols-6 mb-3 bg-white  rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 overflow-hidden'>
+			<div className='col-span-2'>
+				<img
+					className='h-full object-cover object-center'
+					src={image}
+					alt={`Imagen de ${name} en una carta digital`}
+				/>
+			</div>
+
+			<div className='p-4 col-span-4 grid'>
+				<div className='col-span-4 mb-6'>
+					<p className='text-sm text-slate-500'>{category}</p>
+					<h5 className='text-xl font-semibold tracking-tight text-gray-900 dark:text-white'>
+						{name}
+					</h5>
+					<p className=' text-sm text-slate-400'>{description}</p>
+				</div>
+
+				<div className='grid grid-cols-6 col-span-4'>
+					<span className='my-auto col-span-3 text-3xl  font-bold text-gray-900 dark:text-white'>
+						{`$ ${price}`}
+					</span>
+					<a
+						href='#'
+						className='text-white col-span-3 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-3 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800'>
+						Añadir al carrito
+					</a>
+				</div>
+			</div>
+		</div>
+	);
+};
